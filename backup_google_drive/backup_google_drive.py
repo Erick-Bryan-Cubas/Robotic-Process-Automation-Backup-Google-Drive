7a51eded813a9473274e02a7f69542b3cd2fac0c
Erick Bryan Cubas <thecubas@outlook.com>
2023-05-22 03:21:06 -0300

    commit

--- backup_google_drive/backup_google_drive.py
+++ backup_google_drive/backup_google_drive.py
@@ -1,17 +1,37 @@
 import pyautogui
+import time
 
 pyautogui.alert("O código vai começar. Não use nada do seu computador enquanto o código estiver rodando")
+pyautogui.PAUSE = 0.2
 
 # Abrir o Google Drive no computador
 pyautogui.press('winleft')
 pyautogui.write('chrome')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.write("https://drive.google.com/drive/my-drive")
+time.sleep(1)
 pyautogui.press('enter')
 
-
 # Entrar na Área de Trabalho
+pyautogui.hotkey('winleft', 'd')
 
 # Selecionar o arquivo de backup
+# pyautogui.position() / Encontra a posição na tela
+pyautogui.moveTo(114, 111)
+pyautogui.mouseDown()
+time.sleep(1)
+pyautogui.moveTo(912, 589)
 
-# Encaminhar para o Google Drive
+# Anexando o arquivo no Google Drive
+pyautogui.hotkey('alt', 'tab')
+pyautogui.mouseUp()
+
+# Esperar 5 segundos
+time.sleep(5)
+
+pyautogui.alert("O código acabou de rodar. Pode usar o seu computador novamente")
+
 
 # Esperar 5 segundos
